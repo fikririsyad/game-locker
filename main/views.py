f758531b6070d3b0e03139e214ed219f5f635a73
fikririsyad <fikri.risyad10@gmail.com>
2023-09-19 13:27:28 +0700

    Create forms.py, update views.py and urls.py

--- main/views.py
+++ main/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from main.forms import ItemForm
+from main.models import Item
 
 # Create your views here.
 def show_main(request):
+    items = Item.objects.all()
+
     context = {
         'name': 'Fikri Risyad Indratno',
-        'class': 'PBP C'
+        'class': 'PBP C',
+        'items': items
     }
 
     return render(request, "main.html", context)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "create_item.html", context)
