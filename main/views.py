6611ecf78b1fcd32c3af0c47b3a51dd197f8bb31
fikririsyad <fikri.risyad10@gmail.com>
2023-09-19 17:29:29 +0700

    Add functions to show data in views.py

--- main/views.py
+++ main/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from main.forms import ItemForm
 from main.models import Item
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_main(request):
@@ -25,3 +27,19 @@ def create_item(request):
 
     context = {'form': form}
     return render(request, "create_item.html", context)
+
+def show_xml(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
